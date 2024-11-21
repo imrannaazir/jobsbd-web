e7d9425db447f9c956f66d03391eb7ac7734a077
emon02 <abdulalimemon11@gmail.com>
2024-11-22 03:05:19 +0600

    complete footer and not found page and fix some bugs on mobile manu

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import facebook from "@/assets/main/facebook.png";
 import instagram from "@/assets/main/instagram.png";
 import linkedin from "@/assets/main/linkedin.png";
+import { FaPhone, FaLocationDot } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
   const d = new Date();
@@ -11,8 +13,107 @@ const Footer = () => {
   return (
     <section className="bg-[#131525] py-10">
       <Container>
-        <div></div>
-        <div>
+        <div className="py-10">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <div>
+              <p className="footer-title">For Job Seekers</p>
+
+              <div className="flex flex-col items-start mt-5 space-y-2 text-white">
+                <Link href="#" className="footer-link">
+                  Register as Candidate
+                </Link>
+                <Link href="#" className="footer-link">
+                  Login
+                </Link>
+                <Link href="#" className="footer-link">
+                  Find Jobs
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <p className="footer-title">For Employers</p>
+
+              <div className="flex flex-col items-start mt-5 space-y-2 text-white">
+                <Link href="#" className="footer-link">
+                  Post a Job
+                </Link>
+                <Link href="#" className="footer-link">
+                  Register
+                </Link>
+                <Link href="#" className="footer-link">
+                  Recruiter Login
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <p className="footer-title">Quick Link</p>
+
+              <div className="flex flex-col items-start mt-5 space-y-2 text-white">
+                <Link href="#" className="footer-link">
+                  About atB Jobs
+                </Link>
+                <Link href="/contact-us" className="footer-link">
+                  Get Support
+                </Link>
+                <Link href="/terms-conditions" className="footer-link">
+                  Terms and Conditions
+                </Link>
+                <Link href="/privacy-policy" className="footer-link">
+                  Privacy Policy
+                </Link>
+                <Link href="/refund-policy" className="footer-link">
+                  Refund Policy
+                </Link>
+                <Link href="#" className="footer-link">
+                  FAQs For Candidate
+                </Link>
+                <Link href="#" className="footer-link">
+                  FAQs For Company
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <p className="font-bold text-primary text-lg md:text-xl uppercase">
+                Contact Us
+              </p>
+
+              <div className="flex flex-col items-start mt-5 space-y-5">
+                <p className="flex justify-center items-center gap-3">
+                  <span className="text-primary bg-[#1D1E1B] p-3 rounded-full flex items-center justify-center">
+                    <FaPhone className="size-5" />
+                  </span>
+                  <span className="text-lg font-semibold text-white">
+                    +8801234567890
+                  </span>
+                </p>
+                <p className="flex justify-center items-center gap-3">
+                  <span className="text-primary bg-[#1D1E1B] p-3 rounded-full flex items-center justify-center">
+                    <IoMdMail className="size-5" />
+                  </span>
+                  <span className="text-lg font-semibold text-white">
+                    contact@jobsbd.vercel.app
+                  </span>
+                </p>
+                <p className="flex justify-center items-center gap-3">
+                  <span className="text-primary bg-[#1D1E1B] p-3 rounded-full flex items-center justify-center">
+                    <FaLocationDot className="size-5" />
+                  </span>
+                  <span className="text-lg font-semibold text-white">
+                    11th Floor, MMK-Aakash Avenue, Plot No. 12, Kamal Ataturk
+                    Avenue, Gulshan-2, Dhaka-1212, Bangladesh
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+      <div className="border-b border-gray-600"></div>
+      <Container>
+        <div className="pt-10">
           <div className="flex flex-col lg:flex-row gap-5 justify-between">
             <div>
               <p className="text-white">
