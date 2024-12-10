8d124e416b800e72f9c1d38d133a1a1c9e537324
imrannaazir <imrannaaziremon@gmail.com>
2024-12-10 15:56:02 +0600

    fixed: hydration error in the followed button

--- src/components/ui/followed-button.tsx
+++ src/components/ui/followed-button.tsx
@@ -5,7 +5,11 @@ import {
   useGetAllMyFollowedCompanyQuery,
   useToggleCompanyFollowMutation,
 } from "@/redux/api/company/company-api";
+import { selectCurrentUser } from "@/redux/features/auth/authSlice";
+import { useAppSelector } from "@/redux/hooks";
+import { TCompany } from "@/type/company.types";
 import { ClassValue } from "clsx";
+import { PlusCircle } from "lucide-react";
 import { Button } from "./button";
 
 const FollowedButton = ({
@@ -15,21 +19,29 @@ const FollowedButton = ({
   companyId: string;
   className?: ClassValue;
 }) => {
+  const user = useAppSelector(selectCurrentUser);
   const { data, isFetching } = useGetAllMyFollowedCompanyQuery("");
-  const followedCompanyIds =
-    data?.data?.map((item: any) => item?.companyId) || [];
-  const isFollowed = followedCompanyIds?.include(companyId);
+
+  let followedCompanyIds: string[] = [];
+  if (data) {
+    followedCompanyIds = data?.data?.map((item: TCompany) => item?.id);
+  }
+  const isFollowed = followedCompanyIds?.includes(companyId);
   const [toggleFollow, { isLoading }] = useToggleCompanyFollowMutation();
   const handleFlowing = async () => {
     await toggleFollow(companyId);
   };
+
   return (
     <Button
       onClick={handleFlowing}
-      disabled={isLoading || isFetching}
-      className={cn("bg-emerald-500 hover:bg-emerald-600", className)}
+      disabled={isLoading || isFetching || !user?.id}
+      className={cn(
+        "rounded-full bg-blue-100 hover:text-white  text-primary",
+        className
+      )}
     >
-      +{" "}
+      <PlusCircle />
       {isLoading && isFetching ? "Loading" : isFollowed ? "UNFOLLOW" : "FOLLOW"}
     </Button>
   );
